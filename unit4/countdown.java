29d4a15a782723098d7154f586a84a93895ae86e
Student <Student@MS-Lab-3.local>
2023-10-02 08:34:24 -0400

    saving more files

---
+++ unit4/countdown.java
@@ -0,0 +1,15 @@
+public class countdown
+{
+    public static void main(String[] args)
+    {
+        int x=10;
+        while (x>0){
+            System.out.println(x);
+            x=x-1;
+        }
+
+        for (int i=10;i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
