daa5eaa72ac6fcf4d556b770bac9fafc84ef1cea
Student <Student@MS-Lab-3.local>
2024-04-01 14:05:11 -0400

    learning inheritance

---
+++ inheritance/instruments/Instrument.java
@@ -0,0 +1,22 @@
+public class Instrument
+{
+  private String name;
+  private String family;
+
+  public Instrument(String name, String family){
+    this.name = name;
+    this.family = family;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public String getFamily(){
+    return family;
+  }
+
+  public String toString(){
+    return name + " is a member of the " + family + " family.";
+  }
+}
---
+++ inheritance/instruments/Strings.java
@@ -0,0 +1,8 @@
+public class Strings extends Instrument{
+  private boolean bow;
+
+  public Strings(String name, boolean bow){
+    super(name, "Strings");
+    this.bow=bow;
+  } 
+}
