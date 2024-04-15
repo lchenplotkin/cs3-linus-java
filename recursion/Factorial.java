e8cdb5c7a308d99d51cf7fa364625e7fd3d81ade
Student <Student@MS-Lab-3.local>
2024-04-15 09:08:51 -0400

    more recursion

---
+++ recursion/Factorial.java
@@ -0,0 +1,14 @@
+class Factorial {
+  public static void main(String[] args){
+    System.out.println(factorial(10));
+  
+    System.out.println(factorial(30));
+  }
+
+  public static int factorial(int n){
+    if (n==0){
+      return 1;
+    }
+    return n * factorial(n-1);
+  }
+}
